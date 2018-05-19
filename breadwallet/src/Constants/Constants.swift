2be3384b480a7e823a0074ef2ebbf638dbfb00bf
j4hangir <j4hangir@volatileio.com>
2018-05-19 04:33:10 +0200

    fixed placeholders changed some remaining strings from litecoin, etc. to primuscoin

--- breadwallet/src/Constants/Constants.swift
+++ breadwallet/src/Constants/Constants.swift
@@ -33,7 +33,7 @@ struct C {
     static let secondsInDay: TimeInterval = 86400
     static let maxMoney: UInt64 = 84000000*100000000
     static let satoshis: UInt64 = 100000000
-    static let walletQueue = "com.litecoin.walletqueue"
+    static let walletQueue = "com.primuscoin.walletqueue"
     static let btcCurrencyCode = "PM"
     static let null = "(null)"
     static let maxMemoLength = 250
